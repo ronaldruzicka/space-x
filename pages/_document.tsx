e853dbfc4373787aaf0d3a88e6e08c135cb5459d
Ronald Ruzicka <ruzicka.ronald@gmail.com>
2021-02-12 12:07:41 +0100

    add _document page

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Main, NextScript, DocumentContext } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
