e2363e8ce064074eac6324e8ca4a361cbcf00786
Ronald Ruzicka <ruzicka.ronald@gmail.com>
2021-02-12 13:39:37 +0100

    add missing Head to _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Main, NextScript, DocumentContext } from 'next/document'
+import Document, { Html, Main, NextScript, DocumentContext, Head } from 'next/document'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -10,6 +10,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
+        <Head />
         <body>
           <Main />
           <NextScript />
